7c58aeee3befa0bd9bd5d553270b90b0efaeb7b4
Benjamin DESPRETS <benjamin.desprets@epitech.eu>
2023-05-04 14:27:49 +0200

    [UPDATE] remove obsolete code from bootstrap

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,6 @@
 #include "primitives.hpp"
 #include "core.hpp"
 
-Math::Vector3D color(const RayTracer::Ray& ray, const RayTracer::Sphere& sphere) {
-    if (sphere.hits(ray)) {
-        return Math::Vector3D(1, 0, 0); // Red sphere
-    }
-    // Light blue background
-    Math::Vector3D unitDirection = ray.getDirection();
-    unitDirection /= unitDirection.length();
-    double t = 0.5 * (unitDirection.getY() + 1.0);
-    return Math::Vector3D(0.5, 0.7, 1.0) * t + Math::Vector3D(1.0, 1.0, 1.0) * (1.0 - t);
-}
-
 int main(int ac, char **av)
 {
     Core core;
