43f41f749d6a415d0400e8c5905ba424119b76e1
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-05-04 13:54:11 +0200

    add: RGB Parsing & APrimitive Abstract

--- includes/primitives.hpp
+++ includes/primitives.hpp
@@ -8,31 +8,63 @@
 #pragma once
 
 #include "raytracer.hpp"
+#include <iostream>
 
 namespace RayTracer {
     class IPrimitive {
         public:
             IPrimitive() = default;
             virtual ~IPrimitive() = default;
+
             virtual bool hits(const Ray &ray) const = 0;
+
+            virtual void displayPrimitive() const = 0;
     };
-    class Sphere : public IPrimitive {
+
+    typedef struct RGB {
+        int r;
+        int g;
+        int b;
+    } RGB;
+
+    class APrimitive : public IPrimitive {
+        public:
+            APrimitive() : _color({0, 0, 0}) {};
+            APrimitive(RGB color) : _color(color) {};
+            virtual ~APrimitive() = default;
+
+            //* Getters
+            RGB getColor() const {
+                return _color;
+            }
+
+            //* Setters
+            void setColor(RGB color) {
+                _color = color;
+            }
+
+        protected:
+            RGB _color;
+    };
+
+    class Sphere : public APrimitive {
         public:
             //* Attributes
             Math::Point3D _center;
             double _radius;
 
             //* Constructors
-            Sphere() : _center(), _radius(0) {}
+            Sphere() : _center(), _radius(0), APrimitive({0, 0, 0}) {}
 
-            Sphere(const Math::Point3D &center, double radius)
-                : _center(center), _radius(radius) {}
+            Sphere(const Math::Point3D &center, double radius, RGB color)
+                : _center(center), _radius(radius), APrimitive(color) {}
 
-            Sphere(const Sphere &s) : _center(s._center), _radius(s._radius) {}
+            Sphere(const Sphere &s) : _center(s._center), _radius(s._radius), APrimitive(s._color) {}
 
-            Sphere(Sphere &&s) : _center(s._center), _radius(s._radius) {
+            Sphere(Sphere &&s) : _center(s._center), _radius(s._radius), APrimitive(s._color) {
                 s._center = Math::Point3D();
                 s._radius = 0;
+                s._color = {0, 0, 0};
             }
 
             //* Destructor
@@ -43,6 +75,7 @@ namespace RayTracer {
                 if (this != &s) {
                     _center = s._center;
                     _radius = s._radius;
+                    _color = s._color;
                 }
                 return *this;
             }
@@ -51,8 +84,10 @@ namespace RayTracer {
                 if (this != &s) {
                     _center = s._center;
                     _radius = s._radius;
+                    _color = s._color;
                     s._center = Math::Point3D();
                     s._radius = 0;
+                    s._color = {0, 0, 0};
                 }
                 return *this;
             }
@@ -65,6 +100,13 @@ namespace RayTracer {
                 double c = oc.dot(oc) - _radius * _radius;
                 double discriminant = b * b - 4 * a * c;
                 return (discriminant >= 0);
+            }
+
+            void displayPrimitive() const {
+                std::cout << "[SPHERE]" << std::endl;
+                std::cout << "Center : {" << _center.x << ", " << _center.y << ", " << _center.z << "}" << std::endl;
+                std::cout << "Radius: " << _radius << std::endl;
+                std::cout << "Color : {" << _color.r << ", " << _color.g << ", " << _color.b << "}" << std::endl;
             }
 
             //* Getters
--- src/scene.cpp
+++ src/scene.cpp
@@ -9,18 +9,19 @@
 #include <iostream>
 
 void Core::display_scene(void) {
-    std::cout << "Camera : " << std::endl;
-    // std::cout << "Position : " << _camera._position << std::endl;
-    // std::cout << "Direction : " << _camera._direction << std::endl;
+    std::cout << "\n -- Camera --" << std::endl;
+    std::cout << "Origin : {" << _camera._origin.x << ", " << _camera._origin.y << ", " << _camera._origin.z << "}" << std::endl;
+    std::cout << "Screen Origin : {" << _camera._screen._origin.x << ", " << _camera._screen._origin.y << ", " << _camera._screen._origin.z << "}" << std::endl;
+    std::cout << "Screen Bottom Side : {" << _camera._screen._bottom_side.x << ", " << _camera._screen._bottom_side.y << ", " << _camera._screen._bottom_side.z << "}" << std::endl;
+    std::cout << "Screen Left Side : {" << _camera._screen._left_side.x << ", " << _camera._screen._left_side.y << ", " << _camera._screen._left_side.z << "}" << std::endl;
     std::cout << "Field of view : " << _camera._fov << std::endl;
     std::cout << "Width : " << _camera._width << std::endl;
     std::cout << "Height : " << _camera._height << std::endl;
 
     int nb_prim = _primitives.size();
-    std::cout << "Number of primitives : " << nb_prim << std::endl;
+    std::cout << "\n -- Number of primitives : " << nb_prim << std::endl;
     for (int i = 0; i < nb_prim; i++) {
         std::cout << "Primitive " << i << " : " << std::endl;
-        // std::cout << "Center : " << _primitives[i]->_center << std::endl;
-        // std::cout << "Radius : " << _primitives[i]->_radius << std::endl;
+        _primitives[i]->displayPrimitive();
     }
 }
