a76dd596c436febdb9b567336a14b061af8a084d
Benjamin DESPRETS <benjamin.desprets@epitech.eu>
2023-05-05 15:37:02 +0200

    [UPDATE] advancements to camera setup and properties

--- src/file_parser.cpp
+++ src/file_parser.cpp
@@ -10,6 +10,11 @@
 #include "core.hpp"
 #include "primitives.hpp"
 
+double toRadians(double degrees)
+{
+    return degrees * (3.14159265358979323846 / 180.0);
+}
+
 void parse_camera(const libconfig::Setting &root, Core *core)
 {
     const libconfig::Setting &camera = root["camera"];
@@ -17,10 +22,21 @@ void parse_camera(const libconfig::Setting &root, Core *core)
     const libconfig::Setting &position = camera["position"];
     const libconfig::Setting &rotation = camera["rotation"];
 
+
+    core->_camera.setDistanceToScreen(1.0);
+    core->_camera.setDirection(Math::Vector3D(static_cast<int>(rotation["x"]), static_cast<int>(rotation["y"]), static_cast<int>(rotation["z"])));
     core->_camera.setWidth(static_cast<int>(resolution["width"]));
     core->_camera.setHeight(static_cast<int>(resolution["height"]));
     core->_camera.setOrigin(Math::Point3D(static_cast<int>(position["x"]), static_cast<int>(position["y"]), static_cast<int>(position["z"])));
     core->_camera.setFov(static_cast<double>(camera["fieldOfView"]));
+    
+    
+    double halfScreenHeight = tan(toRadians(core->_camera.getFov() / 2.0)) * core->_camera.getDistanceToScreen();
+    float aspect_ratio = static_cast<float>(core->_camera.getWidth()) / static_cast<float>(core->_camera.getHeight());
+    double screen_height = 2.0 * halfScreenHeight;
+    double screen_width = screen_height * aspect_ratio;
+    Math::Rectangle3D screen(Math::Point3D(-screen_width/2, -screen_height/2, -1), Math::Vector3D(screen_width, 0, 0), Math::Vector3D(0, screen_height, 0));
+    core->_camera.setScreen(screen);
 }
 
 void parse_primitives(const libconfig::Setting &root, Core *core)
