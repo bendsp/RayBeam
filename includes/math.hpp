b5f2b986b3d22e9ccccd6c57f80afd3dac1f0483
Benjamin DESPRETS <benjamin.desprets@epitech.eu>
2023-05-08 14:53:39 +0200

    [ADD] Math::Vector3D crossProduct()

--- includes/math.hpp
+++ includes/math.hpp
@@ -365,4 +365,10 @@ class Math {
                 _left_side = left_side;
             }
     };
+    Vector3D crossProduct(const Vector3D &v1, const Vector3D &v2)
+    {
+        return Vector3D(v1.y * v2.z - v1.z * v2.y,
+                        v1.z * v2.x - v1.x * v2.z,
+                        v1.x * v2.y - v1.y * v2.x);
+    }
 };
