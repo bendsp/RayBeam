5d2d1e19cfb04287bd350bd3902eaf395ac8ef33
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-05-08 11:59:27 +0200

    add: parse translation function

--- src/parser/parse_transformations.cpp
+++ src/parser/parse_transformations.cpp
@@ -7,20 +7,35 @@
 
 #include "core.hpp"
 
-// TODO: parse Transformations
+void parseTranslation(const libconfig::Setting &transformations, Core *core)
+{
+    try {
+        const libconfig::Setting &translations = transformations["translations"];
+
+        for (int i = 0; i < translations.getLength(); ++i) {
+            const libconfig::Setting& translation = translations[i];
+            int index = static_cast<int>(translation["index"]);
+            int x = static_cast<int>(translation["x"]);
+            int y = static_cast<int>(translation["y"]);
+            int z = static_cast<int>(translation["z"]);
+            std::cout << "Translation: " << index << " > " << x << " " << y << " " << z << std::endl;
+            //Math::Vector3D vec = Math::Vector3D(x, y, z);
+            //core->addTransformation(new RayTracer::Translation(vec));
+        }
+    } catch (const libconfig::SettingNotFoundException &nfex) {
+    }
+}
+
+void parseRotation(const libconfig::Setting &transformations, Core *core)
+{
+
+}
+
+// TODO: parse and apply transformations
 void parseTransformations(const libconfig::Setting &root, Core *core)
 {
     const libconfig::Setting &transformations = root["transformations"];
-    const libconfig::Setting &translations = transformations["translations"];
     // const libconfig::Setting &rotations = transformations["rotations"];
 
-    for (int i = 0; i < translations.getLength(); ++i) {
-        const libconfig::Setting& translation = translations[i];
-        int x = static_cast<int>(translation["x"]);
-        int y = static_cast<int>(translation["y"]);
-        int z = static_cast<int>(translation["z"]);
-        std::cout << "Translation: " << x << " " << y << " " << z << std::endl;
-        //Math::Vector3D vec = Math::Vector3D(x, y, z);
-        //core->addTransformation(new RayTracer::Translation(vec));
-    }
+    parseTranslation(transformations, core);
 }
